99154e332ee2c0a180c0272c4d78d554fd8fd26d
Andrzej Bułeczka <andrzejbuleczka@gmail.com>
2023-07-12 21:31:53 +0200

    add test to form

--- rtl-tutorial/src/UserForm.test.js
+++ rtl-tutorial/src/UserForm.test.js
@@ -1,17 +1,32 @@
-import { render, screen } from '@testing-library/react';
-import user from '@testing-library/user-event';
-import UserForm from './UserForm';
+import { render, screen } from "@testing-library/react";
+import user from "@testing-library/user-event";
+import UserForm from "./UserForm";
 
-test('it shows two inputs and a button', () => {
+test("it shows two inputs and a button", () => {
   // render the component
-  render(<UserForm />);
+  render(<UserForm onUserAdd={() => {}} />);
 
   // Manipulate the component or find an element in it
-  const inputs = screen.getAllByRole('textbox');
-  const button = screen.getByRole('button');
+  const inputs = screen.getAllByRole("textbox");
+  const button = screen.getByRole("button");
 
   // Assertion - make sure the component is doing
   // what we expect it to do
   expect(inputs).toHaveLength(2);
   expect(button).toBeInTheDocument();
+});
+
+test("it calls onUserAdd when the form is submitted", () => {
+  render(<UserForm onUserAdd={() => {}} />);
+
+  const [nameInput, emailInput] = screen.getAllByRole("textbox");
+
+  user.click(nameInput);
+  user.keyboard("asia");
+
+  user.click(emailInput);
+  user.keyboard("asia@mail.com");
+
+  const button = screen.getByRole("button");
+  user.click(button);
 });
