cded4c4287bc0acbcb9571825d78cb2a2ef3340d
Andrzej Bułeczka <andrzej.buleczka@onwelo.com>
2023-07-17 14:36:39 +0200

    add basic test and router context

---
+++ codesplain-starter/src/components/repositories/RepositoriesListItem.test.js
@@ -0,0 +1,24 @@
+import { screen, render } from "@testing-library/react";
+import { MemoryRouter } from "react-router";
+import RepositoriesListItem from "./RepositoriesListItem";
+
+function renderComponent() {
+  const repository = {
+    full_name: "facebook/react",
+    language: "JavaScript",
+    description: "A JS library",
+    owner: "facebook",
+    name: "react",
+    html_url: "https://github.com/facebook/react",
+  };
+
+  render(
+    <MemoryRouter>
+      <RepositoriesListItem repository={repository} />
+    </MemoryRouter>
+  );
+}
+
+test("shows a link to github homepage of this repository", () => {
+  renderComponent();
+});
--- codesplain-starter/src/components/repositories/RepositoriesSummary.test.js
+++ codesplain-starter/src/components/repositories/RepositoriesSummary.test.js
@@ -13,7 +13,9 @@ test("displays information about the repository", () => {
   for (let key in repository) {
     const value = repository[key];
     const element = screen.getByText(new RegExp(value));
-
+  
     expect(element).toBeInTheDocument();
   }
 });
+
+
